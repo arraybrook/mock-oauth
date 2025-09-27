9fa07cc5f30ef4221eb4beec9a2611993c461bf8
Cong-Xin Cynthia Qiu <ozooxo@gmail.com>
2025-09-26 20:43:08 -0400

    Fix JWKS

--- app/jwk.ts
+++ app/jwk.ts
@@ -1,6 +1,23 @@
-import { generateKeyPair } from 'jose';
+import { generateKeyPair, importJWK, CryptoKey } from 'jose';
 
-export const { publicKey, privateKey } = await generateKeyPair('RS256', {
-  modulusLength: 2048, // recommended key size
-  extractable: true,
-});
+export let publicKey: CryptoKey | Uint8Array<ArrayBufferLike>;
+export let privateKey: CryptoKey | Uint8Array<ArrayBufferLike>;
+
+if (process.env.JWK_PUBLIC_KEY != undefined && process.env.JWK_PRIVATE_KEY != undefined) {
+  publicKey = await importJWK(
+    JSON.parse(process.env.JWK_PUBLIC_KEY),
+    "RS256"
+  );
+
+  privateKey = await importJWK(
+    JSON.parse(process.env.JWK_PRIVATE_KEY),
+    "RS256"
+  );
+} else {
+  const keyPair = await generateKeyPair('RS256', {
+    modulusLength: 2048, // recommended key size
+    extractable: true,
+  });
+  publicKey = keyPair.publicKey
+  privateKey = keyPair.privateKey
+}
